a716e3e79000988650a7c840e3d24f366e6b2e75
itlbv <artem.v.fokin@gmail.com>
2019-10-30 20:45:59 +0100

    Add raw table population to Routines container

--- src/Routines/Routines.js
+++ src/Routines/Routines.js
@@ -1,15 +1,33 @@
 import React, {Component} from 'react';
 import classes from './Routines.module.css';
 import RoutinesTable from './RoutinesTable/RoutinesTable';
+import axios from '../axios';
 
 class Routines extends Component {
 
+    state = {
+        routines: null
+    };
+
+    componentDidMount() {
+        axios.get('/routines')
+            .then(response => {
+                this.setState({routines: response.data});
+            })
+    }
+
     render() {
+
+        let routinesTable = null;
+        if (this.state.routines != null) {
+            routinesTable = <RoutinesTable routines={this.state.routines}/>;
+        }
+
         return (
             <div className={classes.routines}>
                 <h2>Routines</h2>
                 <section>Here's a table of all routines</section>
-                <RoutinesTable/>
+                {routinesTable}
             </div>
         );
     }
--- src/Routines/RoutinesTable/RoutinesTable.js
+++ src/Routines/RoutinesTable/RoutinesTable.js
@@ -1,21 +1,35 @@
 import React from 'react';
 import classes from './RoutinesTable.module.css';
+import InRowButton from '../../UI/TableButtons/InRowButton/InRowButton';
 
-const routinesTable = () => (
-    <table className={classes.routinesTable}>
-        <tr>
-            <th>#</th>
-            <th>1</th>
-            <th>2</th>
-            <th>3</th>
+const routinesTable = props => {
+
+    const table = props.routines.map(r => (
+        <tr key={r.id}>
+            <td>{r.id}</td>
+            <td>{r.name}</td>
+            <td>{r.description}</td>
+            <td><InRowButton>Edit</InRowButton></td>
+            <td><InRowButton>Delete</InRowButton></td>
         </tr>
-        <tr>
-            <td>1</td>
-            <td>1</td>
-            <td>1</td>
-            <td>1</td>
-        </tr>
-    </table>
-);
+    ));
+
+    return (
+        <table className={classes.routinesTable}>
+            <thead>
+            <tr>
+                <th>#</th>
+                <th>1</th>
+                <th>2</th>
+                <th>3</th>
+                <th>4</th>
+            </tr>
+            </thead>
+            <tbody>
+            {table}
+            </tbody>
+        </table>
+    );
+};
 
 export default routinesTable;
