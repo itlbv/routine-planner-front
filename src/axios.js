17d58696a08cc852ab3ef793fcd6e7deef955c84
itlbv <artem.v.fokin@gmail.com>
2019-11-05 19:09:54 +0100

    Change base URL in accordance with backend changes

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:8080/routineplanner/rest'
+    baseURL: 'http://localhost:8080/routineplanner'
 });
 
 export default instance;
