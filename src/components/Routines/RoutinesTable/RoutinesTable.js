4dc8a03a707719e1221f073b82015272766663a8
itlbv <artem.v.fokin@gmail.com>
2019-11-08 12:30:47 +0100

    Add RoutineTable's style

--- src/components/Routines/RoutinesTable/RoutinesTable.js
+++ src/components/Routines/RoutinesTable/RoutinesTable.js
@@ -1,42 +1,45 @@
 import React from 'react';
 import classes from './RoutinesTable.module.css';
 import InTableRowButton from '../../UI/InTableRowButton/InTableRowButton';
+import {AiOutlineDelete, AiOutlineEdit} from 'react-icons/ai'
 
 const routinesTable = props => {
 
     const table = props.routines.map(r => (
         <tr key={r.id}>
-            <td>{r.id}</td>
+            <td>{r.timeOfDay.substr(0, 5)}</td>
             <td>{r.name}</td>
             <td>{r.description}</td>
             <td>
                 <InTableRowButton type={'edit'}
                                   clicked={() => props.edit(r.id)}
-                >Edit</InTableRowButton>
+                ><AiOutlineEdit/></InTableRowButton>
             </td>
             <td>
                 <InTableRowButton type={'delete'}
                                   clicked={() => props.delete(r.id)}
-                >Delete</InTableRowButton>
+                ><AiOutlineDelete/></InTableRowButton>
             </td>
         </tr>
     ));
 
     return (
-        <table className={classes.routinesTable}>
-            <thead>
-            <tr>
-                <th>#</th>
-                <th>1</th>
-                <th>2</th>
-                <th>3</th>
-                <th>4</th>
-            </tr>
-            </thead>
-            <tbody>
-            {table}
-            </tbody>
-        </table>
+        <div className={classes.containerTable}>
+            <table>
+                <thead>
+                <tr>
+                    <th>TIME</th>
+                    <th>TITLE</th>
+                    <th/>
+                    <th/>
+                    <th/>
+                </tr>
+                </thead>
+                <tbody>
+                {table}
+                </tbody>
+            </table>
+        </div>
     );
 };
 
